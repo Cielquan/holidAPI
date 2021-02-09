57c1cafba2335407bab974c449391e20986ef8f5
Christian Riedel <cielquan@protonmail.com>
2021-02-09 17:41:50 +0100

    update models

--- src/models.py
+++ src/models.py
@@ -36,6 +36,27 @@ states = {
 }
 
 
+pub_holidays = {
+    "Neujahr": {"wer": "alle", "wann": "01.01."},
+    "Heilige Drei Könige": {"wer": ["BW", "BY", "ST"], "wann": "06.01."},
+    "Frauentag": {"wer": ["BE"], "wann": "08.03."},
+    "Karfreitag": {"wer": "alle", "wann": "OS - 2"},
+    "Ostermontag": {"wer": "alle", "wann": "OS + 1"},
+    "Tag der Arbeit": {"wer": "alle", "wann": "01.05."},
+    "Christi Himmelfahrt": {"wer": "alle", "wann": "OS + 39"},
+    "Pfingsmontag": {"wer": "alle", "wann": "OS + 50"},
+    "Fronleichnam": {"wer": ["BW", "BY", "HE", "NW", "RP", "SL"], "wann": "OS + 60"},
+    "Mariä Himmelfahrt": {"wer": ["SL"], "wann": "15.08."},
+    "Weltkindertag": {"wer": ["TH"], "wann": "20.09."},
+    "Tag der d. Einheit": {"wer": "alle", "wann": "03.10."},
+    "Reformationstag": {"wer": ["BB", "HB", "HH", "MW", "NI", "SN", "ST", "SH", "TH"], "wann": "31.10."},
+    "Allerheiligen": {"wer": ["BW", "BY", "NW", "RP", "SL"], "wann": "01.11."},
+    "Buß- und Bettag": {"wer": ["SN"], "wann": "Mi vor 23.11."},
+    "1. Weihnachtsfeiertag": {"wer": "alle", "wann": "25.12."},
+    "2. Weihnachtsfeiertag": {"wer": "alle", "wann": "26.12."},
+}
+
+
 engine = db.create_engine("sqlite:///" + str(Path("holiday.db")))
 Session: db.orm.session.Session = sessionmaker(bind=engine)
 Base: DeclarativeMeta = declarative_base()
@@ -83,26 +104,26 @@ class Holiday(Base):
         )
 
 
-class PublicHoliday(Base):
-    """Database model for german public holidays."""
+# class PublicHoliday(Base):
+#     """Database model for german public holidays."""
 
-    __tablename__ = "public_holidays"
+#     __tablename__ = "public_holidays"
 
-    id = db.Column(db.Integer, primary_key=True)  # noqa: VNE003
-    state_id = db.Column(db.Integer, db.ForeignKey("states.id"), nullable=False)
-    holiday = db.Column(db.String(50), index=True, nullable=False)
-    year = db.Column(db.Integer, index=True, nullable=False)
-    school_year = db.Column(db.String(5), index=True, nullable=False)
-    date = db.Column(db.DATE, nullable=False)
+#     id = db.Column(db.Integer, primary_key=True)  # noqa: VNE003
+#     state_id = db.Column(db.Integer, db.ForeignKey("states.id"), nullable=False)
+#     holiday = db.Column(db.String(50), index=True, nullable=False)
+#     year = db.Column(db.Integer, index=True, nullable=False)
+#     school_year = db.Column(db.String(5), index=True, nullable=False)
+#     date = db.Column(db.DATE, nullable=False)
 
-    def __repr__(self) -> str:
-        """Represent PublicHoliday model."""
-        return (
-            f"<public_holidays(public_holiday='{self.holiday}', "
-            f"state='{self.state}', "
-            f"school_year='{self.school_year}', "
-            f"date='{self.date}'>"
-        )
+#     def __repr__(self) -> str:
+#         """Represent PublicHoliday model."""
+#         return (
+#             f"<public_holidays(public_holiday='{self.holiday}', "
+#             f"state='{self.state}', "
+#             f"school_year='{self.school_year}', "
+#             f"date='{self.date}'>"
+#         )
 
 
 def fill_states_table() -> None:
@@ -111,6 +132,14 @@ def fill_states_table() -> None:
     for model in (State(abbr=k, long_name=states[k]) for k in states):
         session.add(model)
     session.commit()
+
+
+def fill_holiday_table() -> None:
+    pass
+
+
+def fill_public_holiday_table() -> None:
+    pass
 
 
 def create_db() -> None:
